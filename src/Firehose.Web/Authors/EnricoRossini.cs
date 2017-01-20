e56d801f1b946dcd9c93921e9b887c8f2cb39ebb
erportico <e.rossini@portico.com>
2017-01-20 10:13:34 +0000

    Update bio :)

--- src/Firehose.Web/Authors/EnricoRossini.cs
+++ src/Firehose.Web/Authors/EnricoRossini.cs
@@ -9,7 +9,7 @@ namespace Firehose.Web.Authors {
     public class EnricoRosini : IAmACommunityMember, IFilterMyBlogPosts {
         public string FirstName => "Enrico";
         public string LastName => "Rossini";
-        public string ShortBioOrTagLine => "I am a strong full-stack developer, from html5/js on the client-side to C# multi-layer server-side code and client-side code. I'm glad to be a Windows 10 and Xamarin Developer!!!";
+        public string ShortBioOrTagLine => "is a .NET full-stack developer. I'm glad to be a Windows 10 and Xamarin Developer!!!";
         public string StateOrRegion => "London";
         public string EmailAddress => "enrico.rossini.uk@live.com";
         public string TwitterHandle => "erossiniuk";
